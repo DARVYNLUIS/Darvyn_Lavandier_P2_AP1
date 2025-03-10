4deec5e805d122df69f5eb2ffdf499ee9d953507
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-03-10 16:59:21 -0400

    Contexto realizado.

---
+++ Darvyn_Lavandier_P2_AP1/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Darvyn_Lavandier_P2_AP1.Models;
+using Microsoft.EntityFrameworkCore;
+using Darvyn_Lavandier_P2_AP1.Models;
+
+
+namespace Darvyn_Lavandier_P2_AP1.DAL
+{
+
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
