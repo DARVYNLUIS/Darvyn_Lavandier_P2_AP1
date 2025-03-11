537542203c8b51b23400b92eb94934f4caba27db
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-03-10 20:59:31 -0400

    Contexto listo.

--- Darvyn_Lavandier_P2_AP1/DAL/Contexto.cs
+++ Darvyn_Lavandier_P2_AP1/DAL/Contexto.cs
@@ -1,13 +1,24 @@
-﻿using Darvyn_Lavandier_P2_AP1.Models;
-using Microsoft.EntityFrameworkCore;
-using Darvyn_Lavandier_P2_AP1.Models;
+﻿using Microsoft.EntityFrameworkCore;
 
-
-namespace Darvyn_Lavandier_P2_AP1.DAL
+namespace Darvyn_Lavandier_P2_AP1.Models
 {
-
     public class Contexto : DbContext
     {
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Encuesta> Encuestas { get; set; }
+        public DbSet<EncuestaDetalle> EncuestaDetalles { get; set; }
+        public DbSet<Ciudad> Ciudades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ciudad>().HasData(
+                new Ciudad { Id = 1, Nombre = "Santo Domingo" },
+                new Ciudad { Id = 2, Nombre = "Santiago" },
+                new Ciudad { Id = 3, Nombre = "San Francisco de Macoris" }
+            );
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
